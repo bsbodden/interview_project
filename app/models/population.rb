6af8e0b5ca33b9ac5c5a6b8eb8ffb580f2319c22
Brian Sam-Bodden <bsbodden@integrallis.com>
2019-12-03 17:03:40 -0700

    refactor: remove unnecessary minimum year parameter check [CH-1]

--- app/models/population.rb
+++ app/models/population.rb
@@ -1,15 +1,6 @@
 class Population < ApplicationRecord
-  def self.min_year
-    Population.minimum(:year)
-  end
-
   def self.get(year)
-    year = year.to_i
-
-    return 0 if year < min_year
-
     pop = Population.where("year <= ?", year).order("year DESC").limit(1).first
-
-    return pop.population if pop
+    pop ? pop.population : 0
   end
 end
