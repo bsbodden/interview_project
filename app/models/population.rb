695f9638d987d0e18a00c8e329d4e405bf6faef9
Brian Sam-Bodden <bsbodden@integrallis.com>
2019-12-03 21:49:17 -0700

    refactor: extract queries and calculations to private methods [CH-2]

--- app/models/population.rb
+++ app/models/population.rb
@@ -3,20 +3,33 @@ class Population < ApplicationRecord
     pop = Population.find_by_year(year)
 
     if pop.nil?
-      lower_bound = Population.where("year <= ?", year).order("year DESC").limit(1).first
-
+      lower_bound = year_range_lower_bound(year)
       if lower_bound.nil?
         0
       else
-        upper_bound = Population.where("year >= ?", year).order("year ASC").limit(1).first
+        upper_bound = year_range_upper_bound(year)
         if upper_bound.nil?
           lower_bound.population
         else
-          lower_bound.population + ((upper_bound.population - lower_bound.population) * ((year.to_f - lower_bound.year.to_f) / (upper_bound.year.to_f - lower_bound.year.to_f)))
+          linear_progression(lower_bound, upper_bound, year)
         end
       end
     else
       pop.population
     end
+  end
+
+  private
+
+  def self.linear_progression(lower_bound, upper_bound, year)
+    lower_bound.population + ((upper_bound.population - lower_bound.population) * ((year.to_f - lower_bound.year.to_f) / (upper_bound.year.to_f - lower_bound.year.to_f)))
+  end
+
+  def self.year_range_upper_bound(year)
+    Population.where("year >= ?", year).order("year ASC").limit(1).first
+  end
+
+  def self.year_range_lower_bound(year)
+    Population.where("year <= ?", year).order("year DESC").limit(1).first
   end
 end
