8f9244fa2c409b74e240bcad1fe62f99a656dd9c
Brian Sam-Bodden <bsbodden@integrallis.com>
2019-12-03 21:27:21 -0700

    feature: return linear progression for years between a known range [CH-2]

--- app/models/population.rb
+++ app/models/population.rb
@@ -1,6 +1,22 @@
 class Population < ApplicationRecord
   def self.get(year)
-    pop = Population.where("year <= ?", year).order("year DESC").limit(1).first
-    pop ? pop.population : 0
+    pop = Population.find_by_year(year)
+
+    if pop.nil?
+      lower_bound = Population.where("year <= ?", year).order("year DESC").limit(1).first
+
+      if lower_bound.nil?
+        0
+      else
+        upper_bound = Population.where("year >= ?", year).order("year ASC").limit(1).first
+        if upper_bound.nil?
+          lower_bound.population
+        else
+          lower_bound.population + ((upper_bound.population - lower_bound.population) * ((year.to_f - lower_bound.year.to_f) / (upper_bound.year.to_f - lower_bound.year.to_f)))
+        end
+      end
+    else
+      pop.population
+    end
   end
 end
--- spec/models/population_spec.rb
+++ spec/models/population_spec.rb
@@ -1,15 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Population, type: :model do
-
   it "should accept a year we know and return the correct population" do
     expect(Population.get(1900)).to eq(76212168)
     expect(Population.get(1990)).to eq(248709873)
   end
 
-  it "should accept a year we don't know and return the previous known population" do
-    expect(Population.get(1902)).to eq(76212168)
-    expect(Population.get(1908)).to eq(76212168)
+  it "should accept a year we don't know and return the previous known population if outside the range of known years" do
+    expect(Population.get(1992)).to eq(248709873)
+    expect(Population.get(1998)).to eq(248709873)
   end
 
   it "should accept a year that is before earliest known and return zero" do
@@ -23,4 +22,12 @@ RSpec.describe Population, type: :model do
     expect(Population.get(200000)).to eq(248709873)
   end
 
+  it "should return a linear progression for years we don't know that are between known years" do
+    expect(Population.get(1955)).to eq(165324486.5)
+    expect(Population.get(1956)).to eq(168124224.2)
+    expect(Population.get(1957)).to eq(170923961.9)
+    expect(Population.get(1975)).to eq(214922115.0)
+    expect(Population.get(1977)).to eq(219570148.6)
+    expect(Population.get(1981)).to eq(228758966.4)
+  end
 end
