b25ef693589abc05aefc00e06d58a2ac3d2fe831
Brian Sam-Bodden <bsbodden@integrallis.com>
2019-12-04 17:57:25 -0700

    feature: auto-refresh the_logz page [CH-9]

--- app/assets/javascripts/populations.js
+++ app/assets/javascripts/populations.js
@@ -1,2 +1,18 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// Copied from https://stackoverflow.com/questions/37358652/how-to-refresh-a-page-with-turbolinks
+var reloadWithTurbolinks = (function() {
+  var scrollPosition;
+
+  function reload() {
+    scrollPosition = [window.scrollX, window.scrollY];
+    Turbolinks.visit(window.location.toString(), { action: "replace" });
+  }
+
+  document.addEventListener("turbolinks:load", function() {
+    if (scrollPosition) {
+      window.scrollTo.apply(window, scrollPosition);
+      scrollPosition = null;
+    }
+  });
+
+  return reload;
+})();
